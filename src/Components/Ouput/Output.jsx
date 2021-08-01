afed362a9d5c8bf84adec01006ba87a64753b214
Akshat Atray <akshatatray31@gmail.com>
2021-08-02 01:01:31 +0530

    Basic Folder Structure 🗂

---
+++ src/Components/Ouput/Output.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Output.css";
+
+const Output = () => {
+    return (
+        <div className="Output">
+            Output!
+        </div>
+    )
+}
+
+export default Output;
