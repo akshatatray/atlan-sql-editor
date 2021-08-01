a563080e3193a754c015b71d54874d9178ffc343
Akshat Atray <akshatatray31@gmail.com>
2021-08-02 01:50:51 +0530

    Navbar Added! ✨

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './Components/Navbar/Navbar';
+
 const App = () => {
   return (
-    <div className="App">
-      App!
+    <div>
+      <Navbar/>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import "./Navbar.css";
 
+import Logo from "./logo.svg";
+
 const Navbar = () => {
     return (
         <div className="Navbar">
-            Navbar!
+            <nav className="Navbar-nav">
+                <div className="Navbar-brand">
+                    <img src={Logo} alt="logo" className="Navbar-logo" />
+                </div>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">New Editor</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Guide</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link nav-link-logout" href="#">Log Out</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
