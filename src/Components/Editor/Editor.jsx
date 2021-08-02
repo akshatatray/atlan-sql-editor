7c04df158ecb71bd544eaa0ccbf1f7f0224f3686
Akshat Atray <akshatatray31@gmail.com>
2021-08-02 10:45:46 +0530

    Basic Editor Section Structure 🕸

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./Editor.css";
 
+import IDE from './IDE';
+
 const Editor = () => {
     return (
         <div className="Editor">
-            Editor!
+            <div className="Editor-Body">
+
+            </div>
+            <div className="Editor-IDE">
+                <IDE />
+            </div>
+            <div className="Editor-Actions">
+
+            </div>
         </div>
     )
 }
---
+++ src/Components/Editor/IDE.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './IDE.css';
+
+const IDE = () => {
+    return (
+        <div className="IDE">
+            IDE!
+        </div>
+    )
+}
+
+export default IDE;
