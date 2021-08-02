ab313e4bea633ade8a39711b0ec5faba529bc4c8
Akshat Atray <akshatatray31@gmail.com>
2021-08-02 11:26:19 +0530

    SQL IDE Added! ✨➕

--- src/Components/Editor/IDE.jsx
+++ src/Components/Editor/IDE.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './IDE.css';
 
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/theme/neat.css';
+
 const IDE = () => {
+    const preCode = '/* \n    ENTER QUERIES TO GET RESULTS \n*/';
     return (
         <div className="IDE">
-            IDE!
+            <CodeMirror 
+                className="CodeMirror"
+                value={preCode}
+                options={{
+                    mode: 'SQL',
+                    theme: 'neat',
+                }}
+            />
         </div>
     )
 }
