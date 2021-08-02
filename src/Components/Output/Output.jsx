598c737cc84d028c132206f7d00b639f9072f6f1
Akshat Atray <akshatatray31@gmail.com>
2021-08-02 20:07:21 +0530

    Basic Output Functioning
    ;

--- src/Components/Output/Output.jsx
+++ src/Components/Output/Output.jsx
@@ -1,17 +1,97 @@
 import React from 'react';
 import "./Output.css";
 
+import { CsvToHtmlTable } from 'react-csv-to-table';
+import customerAll from './csv/customerAll.js';
+import customerLondon from './csv/customerLondon.js';
+
 import Doc from './doc.svg';
 
-const Output = () => {
-    return (
-        <div className="Output">
-            <div className="Output-doc">
-                <img src={Doc} alt="doc"/>
-                <p>The results of your query will appear here.</p>
+const Output = ({ displayQuery }) => {
+    switch (displayQuery) {
+        case "" : return (
+            <div className="Output" style={{ backgroundColor: "#E1E1E1" }}>
+                <div className="Output-doc">
+                    <img src={Doc} alt="doc"/>
+                    <p>The results of your query will appear here.</p>
+                </div>
             </div>
-        </div>
-    )
+        );
+
+        case "SELECT * FROM Atlan;": return (
+            <div className="Output">
+                <div style={{
+                    height: "calc(100% - 30px)",
+                    width: "calc(100% - 30px)",
+                    overflow: "scroll",
+                    overflowY: "scroll",
+                    padding: "15px"
+                }}>
+                    <CsvToHtmlTable
+                        data={customerAll}
+                        csvDelimiter=","
+                    />
+                </div>
+            </div>
+        );
+
+        case "SELECT * FROM Atlan WHERE city='London';": return (
+            <div className="Output">
+                <div style={{
+                    height: "calc(100% - 30px)",
+                    width: "calc(100% - 30px)",
+                    overflow: "scroll",
+                    overflowY: "scroll",
+                    padding: "15px"
+                }}>
+                    <CsvToHtmlTable
+                        data={customerLondon}
+                        csvDelimiter=","
+                    />
+                </div>
+            </div>
+        );
+
+        default : return (
+            <div className="Output" style={{ backgroundColor: "#E1E1E1" }}>
+                <div className="Output-doc">
+                    Please Use the Mock Queries!
+                </div>
+            </div>
+        );
+    }
+    // return (
+    //     <div 
+    //         className="Output"
+    //         style={
+    //             displayQuery !== "" ? 
+    //                 { backgroundColor: "#FFFFFF"} :
+    //                 { backgroundColor: "#F1F1F1" }
+    //         }
+    //     >
+    //         {/* {
+    //             displayQuery === "" ? (
+    //                 <div className="Output-doc">
+    //                     <img src={Doc} alt="doc"/>
+    //                     <p>The results of your query will appear here.</p>
+    //                 </div>
+    //             ) : (
+    //                 <div style={{
+    //                     height: "calc(100% - 30px)",
+    //                     width: "calc(100% - 30px)",
+    //                     overflow: "scroll",
+    //                     overflowY: "scroll",
+    //                     padding: "15px"
+    //                 }}>
+    //                     <CsvToHtmlTable
+    //                         data={customerAll}
+    //                         csvDelimiter=","
+    //                     />
+    //                 </div>
+    //             )
+    //         } */}
+    //     </div>
+    // )
 }
 
 export default Output;
---
+++ src/Components/Output/csv/customerLondon.js
@@ -0,0 +1,9 @@
+const customerLondon = `companyName, contactName
+Around the Horn, Thomas Hardy
+B's Beverages, Victoria Ashworth
+Consolidated Holdings, Elizabeth Brown
+Eastern Connection, Ann Devon
+North/South, Simon Crowther
+Seven Seas Imports, Hari Kumar`
+
+export default customerLondon;
