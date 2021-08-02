42a80ce3202358ca8ff83138ae3f2078402d870b
Akshat Atray <akshatatray31@gmail.com>
2021-08-02 10:05:40 +0530

    Basic Sidebar Added! 🔥

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import "./Sidebar.css";
 
+import Account from './account.svg';
+import Download from './download.svg';
+
 const Sidebar = () => {
     return (
         <div className="Sidebar">
-            Sidebar!
+            <ul className="Sidebar-nav">
+                <li className="Sidebar-navItem">
+                    <a href="#" className="Sidebar-navLink">
+                        <img src={Download} className="Sidebar-Icn" alt="Download" />
+                    </a>
+                </li>
+                <li className="Sidebar-navItem">
+                    <a href="#" className="Sidebar-navLink">
+                        <img src={Account} className="Sidebar-Icn" alt="Account" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
