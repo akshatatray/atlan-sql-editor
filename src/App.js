0db8f0edd50dd51021c5b8d2df287286d0a95284
Akshat Atray <akshatatray31@gmail.com>
2021-08-03 15:51:43 +0530

    React Lazy Components 🐢

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import SqlEditor from './Components/SqlEditor/SqlEditor';
+const Home = lazy(() => import('./Components/Home/Home'));
+const SqlEditor = lazy(() => import('./Components/SqlEditor/SqlEditor'));
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" exact component={Home}/>
-      <Route path="/sql" exact component={SqlEditor}/>
-    </Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Route path="/" exact component={Home}/>
+        <Route path="/sql" exact component={SqlEditor}/>
+      </Router>
+    </Suspense>
   );
 }
 
